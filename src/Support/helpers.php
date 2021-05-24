0305d6b0f0e86f58c71637ea860ce78f5eb68a2f
Angga Ari Wijaya <anggadarkprince@gmail.com>
2021-05-24 08:22:29 +0700

    fix detect chat id for group

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -7,7 +7,7 @@ if (!function_exists('detect_chat_id')) {
 
     function detect_chat_id($chatId)
     {
-        $chatId = str_replace(['-', ' ', '+'], '', $chatId);
+        $chatId = str_replace([' ', '+'], '', $chatId);
         if (strpos($chatId, '-') !== false) {
             if (!(strpos($chatId, '@g.us') !== false)) {
                 $chatId .= '@g.us';
